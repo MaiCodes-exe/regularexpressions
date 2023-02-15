9175529b095ea925881d4f038b2716048fd02619
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-15 21:32:06 +0000

    7th ex

---
+++ exercises/7.js
@@ -0,0 +1,5 @@
+// Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+unRegex.test(exampleStr) // Change this line
+let result = unRegex.test(exampleStr);
