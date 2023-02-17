f28506fea8b187d68aa642fd97947cfbe8841503
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-17 15:32:24 +0000

    30th possible review

---
+++ exercises/30.js
@@ -0,0 +1,7 @@
+// Check For Mixed Grouping of Characters
+
+
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/;
+let result = myRegex.test(myString);
