c6271385bc505b4ddee9eb7645738dd31fcf5fee
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-17 15:01:42 +0000

    23rd ex

---
+++ exercises/23.js
@@ -0,0 +1,8 @@
+// // Match Whitespace
+
+
+// Match Whitespace
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
